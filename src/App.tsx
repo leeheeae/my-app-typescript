21ba5bd915dc005e650df89512c89de2b55f0105
unknown <chromeee@daum.net>
2022-03-09 20:18:03 +0900

    typescript props

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
 
 const App = () => {
-    const [value, setValue] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = event;
-        setValue(value);
-    };
-
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("hello", value);
-    };
-
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={value}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="username"
-                />
-                <button>Login</button>
-            </form>
+            <Container>
+                <H1>하하하</H1>
+            </Container>
         </div>
     );
 };
