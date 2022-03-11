641635fc9c98719edc9bd8358798bc5420dc7f31
leeheeae <chromeee@daum.net>
2022-03-11 19:09:33 +0900

    styled

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,15 +1,86 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-const TItle = styled.h1`
-    color: ${props => props.theme.accentColor}
+const Container = styled.div`
+    padding: 0px 20px;
 `;
 
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-contents: center;
+    align-items: center;
+`;
+
+const CoinsList = styled.ul`
+
+`;
+
+const Coin = styled.li`
+    background-color: #fff;
+    color: ${(props) => props.theme.bgColor};
+    padding: 20px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    a {
+        transition: color 0.5s ease-in;
+    }
+    &:hover {
+        a {
+            color: ${(props) => props.theme.accentColor};
+        }
+    }
+`;
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
+`;
+
+const coins = [
+    {
+        id: "btc-bitcoin",
+        name: "Bitcoin",
+        symbol: "BTC",
+        rank: 1,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+    },
+    {
+        id: "eth-ethereum",
+        name: "Ethereum",
+        symbol: "ETH",
+        rank: 2,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+    },
+    {
+        id: "hex-hex",
+        name: "HEX",
+        symbol: "HEX",
+        rank: 3,
+        is_new: false,
+        is_active: true,
+        type: "token",
+    }
+]
+
 const Coins = () => {
-    return <div>
-        <TItle>
-            Coins
-        </TItle>
-    </div>;
+    return (
+    <Container>
+        <Header>
+            <Title>코인</Title>
+        </Header>
+        <CoinsList>
+            {coins.map(coin => 
+                <Coin key={coin.id}>
+                    <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                </Coin>)
+            }
+        </CoinsList>
+    </Container>);
 };
 
 export default Coins;
